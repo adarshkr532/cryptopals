de2287729fd102e410b68f234f292083361c7954
adarsh <adarshkr532@gmail.com>
2020-04-25 10:58:21 +0530

    Solved 6 challenges

--- set1/5-repeating_key_xor.py
+++ set1/5-repeating_key_xor.py
@@ -1,19 +1,23 @@
 import binascii
 
-def repeating_key_xor(s, key):
-    res = ""
+def to_hex(s):
+    try:
+        int(s, 16)
+        return s
+    except ValueError:
+        return s.encode().hex()
+
+def repeating_key_xor(s, key):     # Returns in hex
+    res = "" 
+    s = to_hex(s)
+    key = to_hex(key)
     for i in range(0, len(s), len(key)):
-        res += chr(ord(s[i])^ord(key[0]))
-        if(i+1 < len(s)):
-            res += chr(ord(s[i+1])^ord(key[1]))
-        if(i+2 < len(s)):
-            res += chr(ord(s[i+2])^ord(key[2]))
-    res = binascii.hexlify(res.encode())
-    res = str(res, 'ascii')
+        for j in range(i, min(len(s), i+len(key))):
+            res += hex(int(s[j], 16)^int(key[j-i], 16))
+    res = res.replace('0x', '')
     return res
 
-if __name__ == '__main__':
-    
+if __name__ == '__main__': 
     inp = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
     out = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
     print(repeating_key_xor(inp, "ICE") == out)
--- set1/6-break_repeating_key_xor.py
+++ set1/6-break_repeating_key_xor.py
@@ -1,6 +1,7 @@
 import binascii
 from base64_to_hex import base64_to_hex
 from detect import detect_single_xor 
+from repeat import repeating_key_xor
 
 def to_hex(s):
     try:
@@ -18,21 +19,33 @@ def hamming_distance(s, t):
         dis += bin(p).count('1')
     return dis
 
+def score(data, keysize):
+    k = 2*keysize
+    n = len(data)//k - 1
+    total = 0
+    for i in range(n):
+        s = data[i*k: i*k + k]
+        t = data[i*k + k: i*k + 2*k]
+        total += hamming_distance(s, t)
+    total = (total/keysize)/n
+    return total
+
 def get_keysize(data):
     l = []
-    for keysize in range(2, 41):
-        s = data[0:keysize*2]
-        t = data[keysize*2:keysize*4]
-        dis = hamming_distance(s, t)
-        dis = (1.0*dis)/keysize
-        l.append(tuple((dis, keysize)))
-    l.sort()
-    return l[:3]
+    mn = 1000000
+    for sz in range(2, 40):
+        val = score(data, sz)
+        if(val < mn):
+            mn = val
+            keysize = sz
+    return keysize
 
-def get_key(data, keysize):           
+def get_key(data, keysize):         # keysize in bytes 
     block = [""]*int(keysize)
-    for i in range(len(data)):
-        block[i%keysize] += data[i]
+    for i in range(0, len(data), 2):
+        id = (i//2)%keysize
+        block[id] += data[i]
+        block[id] += data[i+1]
     key = ""
     for i in range(keysize):
         key += chr(detect_single_xor(block[i])[1])
@@ -42,10 +55,16 @@ if __name__ == '__main__':
     
     with open('6.txt', 'r') as file:
         data = file.read().replace('\n', '')
+    
     data = base64_to_hex(data)
     
     keysize = get_keysize(data)
 
-    for sz in keysize:
-        key = get_key(data, sz[1])
-        print(keysize, key)
+    key = get_key(data, keysize)
+
+    print('Key: ', key, '\n')
+
+    msg = repeating_key_xor(data, key)
+    msg = binascii.unhexlify(msg)
+    msg = str(msg, 'ascii')
+    print ('Message:\n', msg)
