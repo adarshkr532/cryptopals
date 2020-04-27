3910dea16ea5b2fcd0704aca60e3d3727c6d20cb
adarsh <adarshkr532@gmail.com>
2020-04-27 16:52:55 +0530

    Added RoundKeyGenerator

--- set1/7-decrypt_aes_128_ecb.py
+++ set1/7-decrypt_aes_128_ecb.py
@@ -4,7 +4,12 @@
 from base64_to_hex import base64_to_hex
 from BitVector import *
 
-def get_statearray(block):
+rKey = []
+forSBox = []
+invSBox = []
+rcon = [1, 0, 0, 0]
+
+def getStateArray(block):
     state = [[0x0 for i in range(4)] for i in range(4)]
     for i in range(0, len(block), 2):
         p = int(block[i], 16)*16 + int(block[i+1], 16)
@@ -13,24 +18,23 @@ def get_statearray(block):
         state[r][c] = p
     return state
 
-def gen_table():
+def genTable():
     AES_modulus = BitVector(bitstring='100011011')
-    invSBox = []
+    c = BitVector(bitstring='01100011')
     d = BitVector(bitstring='00000101')
     for i in range(0, 256):
+        # For the encryption SBox
+        a = BitVector(intVal = i, size=8).gf_MI(AES_modulus, 8) if i != 0 else BitVector(intVal=0)
+        a1,a2,a3,a4 = [a.deep_copy() for x in range(4)]
+        a ^= (a1 >> 4) ^ (a2 >> 5) ^ (a3 >> 6) ^ (a4 >> 7) ^ c
+        forSBox.append(int(a))
+        # For the decryption SBox
         b = BitVector(intVal = i, size=8)
         b1, b2, b3 = [b.deep_copy() for x in range(3)]
         b = (b1 >> 2) ^ (b2 >> 5) ^ (b3 >> 7) ^ d
         check = b.gf_MI(AES_modulus, 8)
         b = check if isinstance(check, BitVector) else 0
         invSBox.append(int(b))
-    return invSBox
-
-def invShiftRows(state):
-    for i in range(4):
-        for j in range(i):
-            state[i] = state[i][3:] + state[i][:3]
-    return state
 
 def gfmul2(a):                  # Multiply by 2 in Galois Field
     res = (a << 1)
@@ -49,6 +53,39 @@ def fieldMultiply(a, b):        # Multiply two numbers in Galois Field
         print(a, b, res)
     return res
 
+def rangeXor(a, b):
+    c = [0]*4
+    for i in range(4):
+        c[i] = (a[i] ^ b[i])
+    return c
+
+def genRoundKeys(id):
+    ckey = rKey[id+3]
+    ckey = ckey[1:] + ckey[:1]
+    for i in range(4):
+        ckey[i] = forSBox[ckey[i]]
+    ckey = rangeXor(ckey, rcon)
+    for i in range(4):
+        ckey = rangeXor(rKey[id+i], ckey)
+        rKey.append(ckey)
+    rcon[0] = gfmul2(rcon[0])
+    
+def genAllKeys(key):
+    rc = []
+    for i in range(len(key)):
+        rc.append(ord(key[i]))
+        if(len(rc) == 4):
+            rKey.append(rc)
+            rc = []
+    for i in range(10):
+        genRoundKeys(4*i)        
+    
+def invShiftRows(state):
+    for i in range(4):
+        for j in range(i):
+            state[i] = state[i][3:] + state[i][:3]
+    return state
+
 
 def invMixColumns(state):
     mat = [[14, 11, 13, 9], [9, 14, 11, 13], [13, 9, 14, 11], [11, 13, 9, 14]]
@@ -59,7 +96,8 @@ def invMixColumns(state):
                 newState[i][j] = newState[i][j] ^ fieldMultiply(state[k][j], mat[i][k])
     return newState
 
-#def decrypt_aes(s):
+#def decryptAes128ECB(s, key):
+    
 
 
 if __name__ == '__main__':
@@ -69,15 +107,14 @@ if __name__ == '__main__':
     
     data = base64_to_hex(data)
 
-    state = get_statearray("8E4DA1BC9FDC589DD5D5D7D64D7EBDF8")
-    
-    for i in state:
-        for j in i:
-            print(hex(j), end = ' ')
-        print('\n')
+    state = getStateArray("8E4DA1BC9FDC589DD5D5D7D64D7EBDF8")
+   
+    genTable()
 
-    state = invMixColumns(state)
-    for i in state:
+    key = "Thats my Kung Fu"
+    genAllKeys(key)
+    for i in rKey:
         for j in i:
-            print(hex(j), end = ' ')
+            print (hex(j), end=' ')
         print('\n')
+    print(len(rKey))
